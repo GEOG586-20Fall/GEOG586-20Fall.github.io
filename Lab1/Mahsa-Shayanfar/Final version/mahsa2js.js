29116b725b9038ecca4a38486956670420284dc1
Mahsa <mahsa.shayanfar@ucalgary.ca>
2020-10-02 17:49:25 +0330

    final commit
    f.commit

--- Lab1/Mahsa-Shayanfar/Final version/mahsa2js.js
+++ Lab1/Mahsa-Shayanfar/Final version/mahsa2js.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([35.738545, 51.313853], 14);
+﻿var mymap = L.map('mapid').setView([35.738545, 51.313853], 14);
 	   
 	   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
