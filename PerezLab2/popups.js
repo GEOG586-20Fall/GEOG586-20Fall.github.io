6e733d0d7553960401af3ddce5a0a15b72574b6b
valchafia <valsofy98@gmail.om>
2020-10-14 20:20:52 -0600

    Initial changes

---
+++ PerezLab2/popups.js
@@ -0,0 +1,105 @@
+  var sicamous = {
+        "type": "Feature",
+        "properties": {
+            "name": "Sicamous",
+            "amenity": "Town",
+            "popupContent": "<strong> Sicamous, BC </strong><br> Where I met Stickshift (my worst enemy)."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-118.9768,50.8378]
+        }
+        };
+
+
+var van1 = {
+        "type": "Feature",
+        "properties": {
+            "name": "Van",
+            "amenity": "City",
+            "popupContent": "<strong>Stanley Park, Vancouver, BC. AB</strong><br>Where we climbed trees and explored nature."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-123.1443, 49.3043]
+        }
+        };
+
+
+var van2 = {
+        "type": "Feature",
+        "properties": {
+            "name": "VCoffee",
+            "amenity": "City",
+            "popupContent": "<strong>Bean Around the World, Vancouver, BC</strong><br>Where I fell in love with a coconut latte."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-123.1090, 49.2762]
+        }
+        };
+
+var van3 = {
+        "type": "Feature",
+        "properties": {
+            "name": "Granville",
+            "amenity": "City",
+            "popupContent": "<strong>Grandville Island, Vancouver, BC</strong><br>Where  I had top notch fish and chips"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-123.1340,49.2712]
+        }
+        };
+
+var van4 = {
+        "type": "Feature",
+        "properties": {
+            "name": "terminal",
+            "amenity": "Port",
+            "popupContent": "<strong>Tsawwassen Terminal</strong><br>Where we took off for the island."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-123.1304,49.0069]
+        }
+        };
+
+var tofino = {
+        "type": "Feature",
+        "properties": {
+            "name": "Long beach",
+            "amenity": "Beach",
+            "popupContent": "<strong>Long Beach, Tofino, BC</strong><br>Where I surfed until I died."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-125.7538, 49.0689]
+        }
+        };
+
+var ucluelet = {
+        "type": "Feature",
+        "properties": {
+            "name": "Ucluelet",
+            "amenity": "Town",
+            "popupContent": "<strong> Ucluelet, BC</strong><br>Where we stayed and relaxed every night."
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-125.5463,48.9416]
+        }
+        };
+
+var calgary = {
+        "type": "Feature",
+        "properties": {
+            "name": "Calgary",
+            "amenity": "City",
+            "popupContent": "<strong>Calgary, AB</strong><br>Where it all began!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-114.0593, 51.0478]
+        }
+        };
