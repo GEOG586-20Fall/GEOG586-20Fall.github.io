14bc791b72f18851006cbfacb77de4bc7c75d475
NicoleM775 <61521403+NicoleM775@users.noreply.github.com>
2020-10-21 11:47:25 -0600

    syntax errors

--- Lab2/Morrison_Lab2/eateries.js
+++ Lab2/Morrison_Lab2/eateries.js
@@ -4,7 +4,7 @@ var Eateries = {
             {"type":"Feature", 
             "properties":{
                 "name":"Ten Foot ",
-                "popupContent":"<b>Ten Foot Henry</b><br> Hip atmosphere with modern vegtable-anchored plates for sharing. $$$."},
+                "popupContent":"<b>Ten Foot Henry</b><br> Hip atmosphere with modern vegtable-anchored plates for sharing. $$$.",
 				"img":"<img src=https://cdn.where.ca/wp-content/uploads/2018/01/ten-foot-henry-web.jpg>"},
                 "geometry":{
                     "type":"Point",
@@ -13,7 +13,7 @@ var Eateries = {
             {"type":"Feature", 
             "properties":{
                 "name":"bridgetterBar",
-                "popupContent":"<b>Bridgette Bar</b><br> Chef-driven food with a unique lounge atmosphere. $$$$."},
+                "popupContent":"<b>Bridgette Bar</b><br> Chef-driven food with a unique lounge atmosphere. $$$$.",
 				"img": "<img src= https://www.visitcalgary.com/sites/default/files/2020-09/visit_during_covid19_header_2020.jpg>"},
                 "geometry":{
                     "type":"Point",
@@ -22,7 +22,7 @@ var Eateries = {
             {"type":"Feature", 
             "properties":{
                 "name":"UNA",
-                "popupContent":"<b>UNA Pizza and Wine</b><br> Hip local elevated pizzeria. $$."},
+                "popupContent":"<b>UNA Pizza and Wine</b><br> Hip local elevated pizzeria. $$.",
 				"img": "<img src=https://www.narcity.com/u/2020/01/20/a4c5a2a265191e631a204d956a8ddb37.jpg_1200x630.jpg>"},
                 "geometry": {
                     "type": "Point",
@@ -31,7 +31,7 @@ var Eateries = {
             {"type":"Feature", 
             "properties":{
                 "name":"Village",
-                "popupContent":"<b>Village Ice Cream</b><br> Homemade ice cream with a trendy twist. $."},
+                "popupContent":"<b>Village Ice Cream</b><br> Homemade ice cream with a trendy twist. $.",
 				"img": "<img src= https://villageicecream.com/wp-content/uploads/2017/11/IMG_4561.jpg>"},
                 "geometry":{
                     "type":"Point",
@@ -40,7 +40,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "Calcutta",
-                "popupContent": "<b>Calcutta Cricket Club</b><br> Modern Indian Cuisine. $$."},
+                "popupContent": "<b>Calcutta Cricket Club</b><br> Modern Indian Cuisine. $$.",
 				"img": "<img src= https://www.avenuecalgary.com/wp-content/uploads/2017/08/CCC-8198562d.jpeg>"},
                 "geometry": {
                     "type": "Point",
@@ -49,7 +49,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "Sidewalk",
-                "popupContent":"<b>Sidewalk Citizen</b><br> Rustic bakery and cafe. $."},
+                "popupContent":"<b>Sidewalk Citizen</b><br> Rustic bakery and cafe. $.",
 				"img": "<img src= https://cdn.canada247.info/assets/uploads/b96c91b17e4da30f7613b9514362dbca_-alberta-division-no-6-calgary-downtown-sidewalk-citizen-bakeryhtml.jpg>"},
                 "geometry": {
                     "type": "Point",
@@ -58,7 +58,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "Anejo",
-                "popupContent":"<b>Anejo</b><br> Contemperary mexican and famous margaritas. $$."},
+                "popupContent":"<b>Anejo</b><br> Contemperary mexican and famous margaritas. $$.",
 				"img":"<img src=https://anejo.ca/4thstreet/wp-content/uploads/sites/4/2018/03/mexican-food-calgary.jpg>"}, 
                 "geometry": {
                     "type": "Point",
@@ -67,7 +67,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "Nash",
-                "popupContent": "<b>The Nash</b><br> Inventive dishes served in an old 1900s hotel.$$$$ ."},
+                "popupContent": "<b>The Nash</b><br> Inventive dishes served in an old 1900s hotel.$$$$ .",
 				"img":"<img src= https://i1.wp.com/fortwoplz.com/wp-content/uploads/2016/03/the-nash-calgary-3.jpg?resize=640%2C427&ssl=1>"}, 
                 "geometry": {
                     "type": "Point",
@@ -76,7 +76,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "OEB",
-                "popupContent":"<b>OEB</b><br> Very funky Brunch spot. $$."},
+                "popupContent":"<b>OEB</b><br> Very funky Brunch spot. $$.",
 				"img": "<img src= https://eatoeb.com/wp-content/uploads/2018/10/Menu-Page-Header-e1574096414372.jpg>"},
                 "geometry": {
                     "type": "Point",
@@ -85,7 +85,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "clive",
-                "popupContent":"<b>Clive burger</b><br> Locally sourced hamburgers and shakes with a retro vibe.$."},
+                "popupContent":"<b>Clive burger</b><br> Locally sourced hamburgers and shakes with a retro vibe.$.",
 				"img": "<img src= https://images.squarespace-cdn.com/content/v1/5af9b3025ffd202a11b88dd3/1537394588780-NK24205ZSWB1SSG8MRPJ/ke17ZwdGBToddI8pDm48kEpVg-ILAPna1wRh-xAJ9fRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwEv36x-EUL2-BSQ5feDhwGCbXuJBFqZ-erYzVouT8yOb9TwqchglLQOCYTRn7ZGxI/image-asset.jpeg>"},
                 "geometry": {
                     "type": "Point",
@@ -94,7 +94,7 @@ var Eateries = {
 			{"type": "Feature",
             "properties": {
                 "name": "Trolley5",
-                "popupContent": "<b>Trolley 5</b><br> Brews their own beer on site as well as featuring other local Micro-breweries, plus amazing wings. $$."},
+                "popupContent": "<b>Trolley 5</b><br> Brews their own beer on site as well as featuring other local Micro-breweries, plus amazing wings. $$.",
 				"img": "<img src= https://live.staticflickr.com/866/40348386494_60c4d9c146_b.jpg>"}, 
                 "geometry": {
                     "type": "Point",
@@ -103,7 +103,7 @@ var Eateries = {
             {"type": "Feature",
             "properties": {
                 "name": "Sushi",
-                "popupContent": "<b>Point Sushi</b><br> Infamous bullet train sushi bar.$$."},
+                "popupContent": "<b>Point Sushi</b><br> Infamous bullet train sushi bar.$$.",
 				"img": "<img src= https://eatwithshar0n.files.wordpress.com/2018/07/img-20180704-wa0000.jpg?w=750>"},
                 "geometry": {
                     "type": "Point",
