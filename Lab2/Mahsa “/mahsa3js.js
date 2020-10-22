c9737d1baf29d388fd7e71130df945c59fea6b13
Mahsa <mahsa.shayanfar@ucalgary.ca>
2020-10-22 19:03:14 +0330

    final-Lab2

---
+++ Lab2/Mahsa “/mahsa3js.js
@@ -0,0 +1,33 @@
+﻿
+
+
+<script>
+
+
+function Popups(feature, layer) {
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    };
+};
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [51.3203761, 35.7398037]
+    }
+};
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: Popups
+}).addTo(map);
+
+
+
+	</script>
+	
