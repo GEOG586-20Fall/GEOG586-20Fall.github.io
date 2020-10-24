d5cdb323e0690b3ce6e4bacea91e1fff45029eb4
Mitchell C <71371873+mcornish1@users.noreply.github.com>
2020-10-24 15:49:51 -0600

    Create pointsOfInterest.js

---
+++ Lab2/Lab2_MitchellC/pointsOfInterest.js
@@ -0,0 +1,88 @@
+
+var onCampus = {
+		"type" : "FeatureCollection",
+        "features" : [
+            {"type":"Feature", 
+            "places":{
+                "name":"IHouse",
+                "popupContent":"<b>International House</b><br> First Year Residence Building and home to the Campus Service Centre.</br><br> Fun fact: I currently live here!",
+				"img": "<img src= https://www.ctvnews.ca/polopoly_fs/1.5022630.1594682110!/httpImage/image.jpeg_gen/derivatives/landscape_960/image.jpeg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.132901, 51.076162]}},
+							
+			{"type":"Feature", 
+            "places":{
+                "name":"Fitness",
+                "popupContent":"<b>Fitness Centre</b><br> During COVID-19 it is still open to all students.",
+				"img": "<img src= https://active-living.ucalgary.ca/sites/default/files/styles/ucws_image_desktop/public/2019-09/FitnessCentre_011A0053_F%20web%20sq.jpg?itok=V8pvFSLk>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.133443, 51.078065]}},
+					
+			{"type":"Feature", 
+            "places":{
+                "name":"Yamnuska",
+                "popupContent":"<b>Yamnuska Hall</b><br> A first and second year residence building.<br> It has a lot of restaurants that students enjoy, including Starbucks, Domino's, and Subway.",
+				"img": "<img src= https://commissioning.ca/wp-content/uploads/2017/08/277b038d-8a36-498e-b42e-dbec784de1d5.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.136981, 51.074836]}},
+			
+			{"type":"Feature", 
+            "places":{
+                "name":"BusTerm",
+                "popupContent":"<b>Craigie Hall Bus Terminal</b><br> An important transportation hub for residence students and commuter students alike.",
+				"img": "<img src= https://logos-download.com/wp-content/uploads/2019/01/Calgary_Transit_Logo.png>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.127228, 51.076424]}},
+			
+			{"type":"Feature", 
+            "places":{
+                "name":"MacHall",
+                "popupContent":"<b>MacEwan Student Centre</b><br> A place to eat, to shop and to access various campus resources.",
+				"img": "<img src= https://i.cbc.ca/1.3285004.1445563619!/fileImage/httpImage/image.png_gen/derivatives/16x9_780/university-of-calgary-mac-ewan-hall.png>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.131054, 51.078409]}},
+					
+			{"type":"Feature", 
+            "places":{
+                "name":"TFDL",
+                "popupContent":"<b>Taylor Family Digital Library</b><br> The library at which I occasionally study at...not so much this year though.",
+				"img": "<img src= http://www.canaconstruction.com/wp-content/uploads/2015/03/tfdl-1-826x536.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.129835, 51.077386]}},;
+					
+					
+var offCampus = {
+		"type" : "FeatureCollection",
+        "features" : [
+            {"type":"Feature", 
+            "places":{
+                "name":"SaveOnFoods",
+                "popupContent":"<b>Save-On-Foods</b><br> This is a new supermarket that opened in August 2020. It is the closest supermarket to residence!",
+				"img": "<img src= https://ucpg.ca/uploads/2020/08/saveonfoodsmain-1.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.147416, 51.077530]}},
+					
+			{"type":"Feature", 
+            "places":{
+                "name":"LRT",
+                "popupContent":"<b>University LRT Station</b><br> Another important transit hub for students. The Red Line stops here with trains towards Tuscany and Somerset-Bridlewood.",
+				"img": "<img src= https://upload.wikimedia.org/wikipedia/commons/8/8c/University_Ctrain_Station_Calgary.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.123122, 51.080100]}},
+			
+			{"type":"Feature", 
+            "places":{
+                "name":"Ducks",
+                "popupContent":"<b>West Campus Park Duck Pond</b><br> A place that I like to visit if I need to go off campus for a break.",
+				"img": "<img src= https://birdscalgary.files.wordpress.com/2013/04/img_2611-e1366851928650.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[-114.143031, 51.069640]}},;
