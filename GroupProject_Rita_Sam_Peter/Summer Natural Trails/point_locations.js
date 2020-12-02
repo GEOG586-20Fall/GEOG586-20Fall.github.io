d703fb3bf69aab31da0f01a8695adfc4e1d1a231
rita-zoura <61521464+rita-zoura@users.noreply.github.com>
2020-12-02 15:27:01 -0700

    Update point_locations.js

--- GroupProject_Rita_Sam_Peter/Summer Natural Trails/point_locations.js
+++ GroupProject_Rita_Sam_Peter/Summer Natural Trails/point_locations.js
@@ -3,196 +3,196 @@
 	"features": [
 		{
 			{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-117.700915, 52.545310]
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-117.700915, 52.545310]
+				},
+				"properties": {
+					"name": "Athabasca River Rafting",
+					"desc": "From Google Maps"
+				}	
 			},
-			"properties": {
-				"name": "Athabasca River Rafting",
-				"desc": "From Google Maps"
-			}	
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.229442, 51.412928]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.229442, 51.412928]
+				},
+				"properties": {
+					"name": "Lake Louise",
+				}
 			},
-			"properties": {
-				"name": "Lake Louise",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-117.096138, 52.166973]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-117.096138, 52.166973]
+				},
+				"properties": {
+					"name": "Icefield Parkway",
+				}
 			},
-			"properties": {
-				"name": "Icefield Parkway",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.250793, 51.056044]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.250793, 51.056044]
+				},
+				"properties": {
+					"name": "Cave exploration",
+				}
 			},
-			"properties": {
-				"name": "Cave exploration",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-117.224522, 52.220999]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-117.224522, 52.220999]
+				},
+				"properties": {
+					"name": "Columbia Icefield",
+				}
 			},
-			"properties": {
-				"name": "Columbia Icefield",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.826925, 51.289337]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.826925, 51.289337]
+				},
+				"properties": {
+					"name": "Kicking Horse water rafting",
+				}
 			},
-			"properties": {
-				"name": "Kicking Horse water rafting",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.596948, 51.203046]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.596948, 51.203046]
+				},
+				"properties": {
+					"name": "Vie Ferrataa",
+				}
 			},
-			"properties": {
-				"name": "Vie Ferrataa",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.262891 50.468564]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.262891 50.468564]
+				},
+				"properties": {
+					"name": "Paradise Mine",
+				}
 			},
-			"properties": {
-				"name": "Paradise Mine",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.521506, 51.188843]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.521506, 51.188843]
+				},
+				"properties": {
+					"name": "Hoodoos",
+				}
 			},
-			"properties": {
-				"name": "Hoodoos",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.176027, 51.416711]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.176027, 51.416711]
+				},
+				"properties": {
+					"name": "Lake Louise Campground",
+				}
 			},
-			"properties": {
-				"name": "Lake Louise Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.537759, 51.189081]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.537759, 51.189081]
+				},
+				"properties": {
+					"name": "Tunnel Mountain Campground",
+				}
 			},
-			"properties": {
-				"name": "Tunnel Mountain Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.505022, 51.227419]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.505022, 51.227419]
+				},
+				"properties": {
+					"name": "Two Jack Campground",
+				}
 			},
-			"properties": {
-				"name": "Two Jack Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.837298, 51.241768]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.837298, 51.241768]
+				},
+				"properties": {
+					"name": "Johnston Canyon Campground",
+				}
 			},
-			"properties": {
-				"name": "Johnston Canyon Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-115.910949, 51.268166]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-115.910949, 51.268166]
+				},
+				"properties": {
+					"name": "Castle Mountain Campground",
+				}
 			},
-			"properties": {
-				"name": "Castle Mountain Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.036529, 51.327458]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.036529, 51.327458]
+				},
+				"properties": {
+					"name": "Protection Montain Campground",
+				}
 			},
-			"properties": {
-				"name": "Protection Montain Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.331122, 51.630739]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.331122, 51.630739]
+				},
+				"properties": {
+					"name": "Mosquito Creek Campground",
+				}
 			},
-			"properties": {
-				"name": "Mosquito Creek Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.868490, 52.040116]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.868490, 52.040116]
+				},
+				"properties": {
+					"name": "Rampart Creek Campground",
+				}
 			},
-			"properties": {
-				"name": "Rampart Creek Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.581175, 51.799926]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.581175, 51.799926]
+				},
+				"properties": {
+					"name": "Silverhorn Creek Campground",
+				}
 			},
-			"properties": {
-				"name": "Silverhorn Creek Campground",
-			}
-		},
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-116.623118, 51.842324]
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [-116.623118, 51.842324]
+				},
+				"properties": {
+					"name": "Waterfowl Lakes Campground",
+				}
 			},
-			"properties": {
-				"name": "Waterfowl Lakes Campground",
-			}
-		},
 		
 		}
 	]
