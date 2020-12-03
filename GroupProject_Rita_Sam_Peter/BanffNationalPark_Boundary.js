f3a0987e0f7b698b756ff1be4472b3badb6224d6
PeterJiang0129 <71541819+PeterJiang0129@users.noreply.github.com>
2020-12-02 20:58:37 -0700

    Update BanffNationalPark_Boundary.js

--- GroupProject_Rita_Sam_Peter/BanffNationalPark_Boundary.js
+++ GroupProject_Rita_Sam_Peter/BanffNationalPark_Boundary.js
@@ -4,8 +4,9 @@
 		{"type": "Feature",
         "properties": {
             "name": "Banff National Park",
-            "caption": "To be added",
-            "img": ""},
+            "caption": "World-famous national park with extradinary scenaries of Canadian natural
+		        beauty and excellent attractions that are engaging and breathtaking.",
+            "img": "<img src= 'https://static.toiimg.com/photo/55704503.cms'/>"},
         "geometry": {
             "type": "Polygon",
             "coordinates": [[[-115.43168596380265, 51.10638357340508],
